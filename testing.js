c4d9dfdc5e5d1abe03df52b26cfe182285c74cda
Johannes Borgström <johannes.borgstrom@1337.tech>
2022-12-07 15:36:05 +0100

    ip

--- testing.js
+++ testing.js
@@ -1,12 +1,12 @@
 module.exports = async ({ github, context }) => {
   const meta = await github.rest.meta.get();
-  console.log('meta:', meta);
-  const commit = await github.rest.repos.getCommit({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    ref: `${SHA}`,
-  });
-  console.log('commit:', commit);
-  core.exportVariable('author', commit.data.commit.author.email);
+  console.log('Ip ranges for actions:', meta.data.actions);
+  // const commit = await github.rest.repos.getCommit({
+  //   owner: context.repo.owner,
+  //   repo: context.repo.repo,
+  //   ref: `${SHA}`,
+  // });
+  // console.log('commit:', commit);
+  // core.exportVariable('author', commit.data.commit.author.email);
   return context.payload.client_payload.value;
 };
