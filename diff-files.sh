e0144c9801a6d0f553acf5b51eebfa8069007e41
Johannes Borgström <johannes.borgstrom@1337.tech>
2023-03-08 20:01:22 +0100

    what

--- diff-files.sh
+++ diff-files.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-echo "CHANGED_FILES=$(git diff --name-only HEAD^ HEAD | grep ^$1\/ -c)" >> $GITHUB_ENV;
+# echo "CHANGED_FILES=$(git diff --name-only HEAD^ HEAD | grep ^$1\/ -c)" >> $GITHUB_ENV;
+
+REGEX_STR=""
+while getopts ":p::" arg; do
+  case "${arg}" in
+    p)
+      if [[ $OPTIND > 3 ]]; then
+        REGEX_STR+="|"
+      fi
+      REGEX_STR+="^$OPTARG\/"
+      ;;
+  esac
+done
+
+echo $REGEX_STR;
+echo "CHANGED_FILES=$(git diff --name-only HEAD^ HEAD | grep $REGEX_STR -c)";
+#  >> $GITHUB_ENV;
