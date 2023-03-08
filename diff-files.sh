d69a6cfb4af2f5455404528adb909c1279646d2f
Johannes Borgström <johannes.borgstrom@1337.tech>
2023-03-08 17:39:38 +0100

    condizionale3

--- diff-files.sh
+++ diff-files.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-# echo "folder prefix: $1"
-CHANGED_FILES=$(git diff --name-only HEAD^ HEAD | grep ^$1\/ -c)
-echo "SOMETHING=123" >> $GITHUB_ENV;
-echo $CHANGED_FILES;
-
-
-# if [[ $CHANGED_FILES > 0 ]]; then
-#   echo "something changed: $CHANGED_FILES"
-# else
-#   echo "nothing changed: $CHANGED_FILES"
-# fi
-
-# echo "done"
+echo "CHANGED_FILES=$(git diff --name-only HEAD^ HEAD | grep ^$1\/ -c)" >> $GITHUB_ENV;
