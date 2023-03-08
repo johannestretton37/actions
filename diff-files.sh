422c9feb7e76983cb2e2cec71f61d98466f57bdb
Johannes Borgström <johannes.borgstrom@1337.tech>
2023-03-08 16:59:51 +0100

    change

---
+++ diff-files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "folder prefix: $1"
+CHANGED_FILES=$(git diff --name-only HEAD^ HEAD | grep ^$1\/ -c)
+
+if [[ $CHANGED_FILES > 0 ]]; then
+  echo "something changed: $CHANGED_FILES"
+else
+  echo "nothing changed: $CHANGED_FILES"
+fi
+
+echo "done"
