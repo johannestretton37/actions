00cbebc9d39984765969e6b9a659f9389b920ab7
Johannes Borgström <johannes.borgstrom@1337.tech>
2023-03-09 09:41:15 +0100

    Check sha

--- diff-files.sh
+++ diff-files.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 REGEX_STR=""
-while getopts ":p::" arg; do
+OLD_COMMIT="HEAD^"
+
+while getopts ":p::c::" arg; do
   case "${arg}" in
     p)
       if [[ $OPTIND > 3 ]]; then
@@ -9,8 +11,17 @@ while getopts ":p::" arg; do
       fi
       REGEX_STR+="^$(echo $OPTARG | sed -E 's/.?\///')\/"
       ;;
+    c)
+      OLD_COMMIT="$OPTARG^"
+      ;;
   esac
 done
 echo $REGEX_STR;
 
-echo "CHANGED_FILES=$(git diff --name-only HEAD^ HEAD | egrep $REGEX_STR -c)"; # >> $GITHUB_ENV;
+NEW_COMMIT=HEAD
+
+echo "git diff --name-only $OLD_COMMIT..$NEW_COMMIT | egrep $REGEX_STR -c"
+echo -e "\n------ CHANGED FILES: ------"
+git diff --name-only $OLD_COMMIT..$NEW_COMMIT | egrep $REGEX_STR;
+echo -e "----------- END ------------\n"
+echo "CHANGED_FILES=$(git diff --name-only $OLD_COMMIT..$NEW_COMMIT | egrep $REGEX_STR -c)"; # >> $GITHUB_ENV;
