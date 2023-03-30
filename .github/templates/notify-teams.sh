1f3c4c5ddb6e6ad7e03f4736d9b4f6fbe802c80d
Johannes Borgström <johannes.borgstrom@1337.tech>
2023-03-30 17:32:03 +0200

    export

--- .github/templates/notify-teams.sh
+++ .github/templates/notify-teams.sh
@@ -11,4 +11,4 @@ export URL_PROD_NO=$(echo $DICT | jq -r '.prod.no');
 export URL_PROD_DK=$(echo $DICT | jq -r '.prod.dk');
 export URL_PROD_FI=$(echo $DICT | jq -r '.prod.fi');
 
-envsubst < ./notify-teams-template.json
+envsubst < ./.github/templates/notify-teams-template.json
