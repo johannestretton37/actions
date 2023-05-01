f00fd0c8a86365c2e14cee89267ba04f977cf402
Johannes Borgström <johannes.borgstrom@1337.tech>
2023-05-01 20:50:10 +0200

    debug

--- scripts/release.ts
+++ scripts/release.ts
@@ -2,16 +2,18 @@ import semver, { ReleaseType } from 'semver';
 import { execSync } from 'child_process';
 import { readdirSync } from 'node:fs';
 import inquirer from 'inquirer';
+import path from 'path';
 
 const VERSION_TYPES: ReleaseType[] = ['patch', 'minor', 'major']; // Not exhaustive at the moment
 
-const [versionType = VERSION_TYPES[0]] = process.argv.slice(2);
-
-const releaseType: ReleaseType = isReleaseType(versionType)
-  ? versionType
-  : 'patch';
-
 async function main() {
+  const pkgJson = getPkgVersion();
+  console.log(`✨Using package.json located at:\n  \x1b[36m${pkgJson.pkgPath}\x1b[0m
+  Make sure this is correct!
+  
+  To use another file, pass a relative or absolute path as an argument, e.g.
+  \x1b[100mnpm run release ../../package.json\x1b[0m\n`);
+
   const gitStatus = execSync('git status --porcelain', {
     encoding: 'utf-8',
   });
@@ -19,7 +21,7 @@ async function main() {
   if (!isGitClean) {
     console.error('\x1b[41m%s\x1b[0m', 'Error:');
     console.error(
-      `Git working directory not clean.\nCommit or reset the following files, then try again:`
+      `  Git working directory not clean.\n  Commit or reset the following files, then try again:`
     );
     console.log('\x1b[33m%s\x1b[0m', `${gitStatus}`);
     process.exit(0);
@@ -34,7 +36,7 @@ async function main() {
         name: 'releaseType',
         type: 'list',
         message: 'What type of release is this?',
-        default: releaseType,
+        default: VERSION_TYPES[0],
         choices: VERSION_TYPES,
       },
       {
@@ -70,18 +72,30 @@ async function main() {
              */
             if (semver.lt(currentVersion, currentTagVersion)) {
               execSync(
-                `npm version ${currentTagVersion} --git-tag-version=false && git add ${pkgPath} && git commit --amend --no-edit && git push`
+                `npm version ${currentTagVersion} --git-tag-version=false`,
+                { encoding: 'utf-8', stdio: 'inherit' }
               );
+              execSync(`git add .`, { encoding: 'utf-8', stdio: 'inherit' });
+              execSync(`git commit -m "Update npm version"`, {
+                encoding: 'utf-8',
+                stdio: 'inherit',
+              });
+              execSync(`git push origin`, {
+                encoding: 'utf-8',
+                stdio: 'inherit',
+              });
             }
           }
 
-          console.log('✨Bumping npm version and creating git tag');
-          execSync(`npm version ${releaseType}`, {
+          console.log(
+            `✨Bumping npm version (${answers.releaseType}) and creating git tag`
+          );
+          execSync(`npm version ${answers.releaseType}`, {
             encoding: 'utf-8',
             stdio: 'inherit',
           });
           console.log('✨Pushing new version');
-          execSync(`git push`, {
+          execSync(`git push origin`, {
             encoding: 'utf-8',
             stdio: 'inherit',
           });
@@ -90,6 +104,7 @@ async function main() {
             encoding: 'utf-8',
             stdio: 'inherit',
           });
+
           const repoName = execSync(`git config --get remote.origin.url`, {
             encoding: 'utf-8',
           }).match(/([a-z0-9_-]*\/[a-z0-9_-]*)/);
@@ -115,105 +130,6 @@ async function main() {
     });
 
   process.exit(0);
-  //   const currentTag = getCurrentTag();
-
-  //   const { path, version: currentVersion } = getPkgVersion();
-  //   let newVersion = semver.inc(currentVersion, releaseType);
-  //   const newTag = `v${semver.inc(currentTag, releaseType)}`;
-  //   if (`v${newVersion}` !== newTag) {
-  //     const shouldUpdateTag = semver.gt(newVersion ?? '', newTag);
-  //     await inquirer
-  //       .prompt([
-  //         {
-  //           type: 'confirm',
-  //           name: 'performUpdate',
-  //           message: () => {
-  //             const message = `Git tag and package version mismatch. Found:
-  //     package version: ${currentVersion}  (${path})
-  //     git tag:         ${currentTag}`;
-  //             const suggestion = shouldUpdateTag
-  //               ? `Create and push a new annotated git tag?`
-  //               : `Update package.json version to ${currentTag.replace('v', '')}?`;
-
-  //             return message + '\n\n  ' + suggestion;
-  //           },
-  //         },
-  //       ])
-  //       .then((answers) => {
-  //         if (!answers.performUpdate) {
-  //           console.log('No actions performed');
-  //           process.exit(0);
-  //         }
-  //         if (shouldUpdateTag) {
-  //           execSync(
-  //             `git tag -a v${currentVersion} -m "Release v${currentVersion}" && git push --tags`,
-  //             { encoding: 'utf-8', stdio: 'inherit' }
-  //           );
-  //         } else {
-  //           execSync(
-  //             `npm version ${currentTag.replace(
-  //               'v',
-  //               ''
-  //             )} --git-tag-version=false && git add ${path} && git commit --amend --no-edit`,
-  //             { encoding: 'utf-8', stdio: 'inherit' }
-  //           );
-  //           newVersion = currentTag.replace('v', '');
-  //         }
-  //       });
-  //   }
-  //   await inquirer
-  //     .prompt([
-  //       {
-  //         name: 'releaseType',
-  //         type: 'list',
-  //         message: 'What type of release is this?',
-  //         default: releaseType,
-  //         choices: VERSION_TYPES,
-  //       },
-  //       {
-  //         name: 'confirm',
-  //         type: 'confirm',
-  //         message: `Actions planned:\n
-  //   ✔︎ Bump package.json version from: ${currentVersion} -> ${newVersion}
-  //   ✔︎ Create and push git tag: ${newTag}
-
-  //   Is this OK?
-  // `,
-  //       },
-  //     ])
-  //     .then((answers) => {
-  //       const { releaseType, confirm } = answers;
-
-  //       if (!confirm) {
-  //         console.log('\x1b[36m%s\x1b[0m', '\nNo actions performed\n');
-  //         process.exit(0);
-  //       }
-
-  //       try {
-  //         console.log('Bumping npm version and creating git tag');
-  //         execSync(`npm version ${releaseType}`, {
-  //           encoding: 'utf-8',
-  //           stdio: 'inherit',
-  //         });
-  //         console.log('Pushing git tag');
-  //         execSync(`git push origin --tags`, {
-  //           encoding: 'utf-8',
-  //           stdio: 'inherit',
-  //         });
-  //         const repoName = execSync(`git config --get remote.origin.url`, {
-  //           encoding: 'utf-8',
-  //         }).match(/([a-z0-9_-]*\/[a-z0-9_-]*)/);
-  //         if (repoName?.length) {
-  //           console.log(
-  //             `Tag created successfully\n\n  Create new release:\n  https://github.com/${repoName[0]}/releases/new?tag=${newTag}\n`
-  //           );
-  //         }
-  //         process.exit(0);
-  //       } catch (err) {
-  //         console.error(err);
-  //         process.exit(1);
-  //       }
-  //     });
 }
 
 main();
@@ -237,7 +153,11 @@ function getCurrentTag() {
 }
 
 function getPkgVersion() {
-  const pkgPath = getPackageJsonPath();
+  const pkgJsonArg = process.argv
+    .slice(2)
+    .find((arg) => arg.endsWith('package.json'));
+  const pkgPath = pkgJsonArg ? path.resolve(pkgJsonArg) : getPackageJsonPath();
+
   if (!pkgPath) {
     console.error('Could not find a package.json file');
     process.exit(1);
@@ -252,7 +172,6 @@ function getPackageJsonPath(dir: string = __dirname): string | null {
   const files = readdirSync(dir);
   const pkgJson = files.find((filename) => filename === 'package.json');
   if (pkgJson) {
-    // console.log(`Using version from ${dir}/package.json`);
     return `${dir}/package.json`;
   }
   const parentDir = dir.split('/').slice(0, -1).join('/');
